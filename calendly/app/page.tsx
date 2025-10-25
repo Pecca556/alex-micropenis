3b3af59317f6eed76f27cded433b35bf3f15e4fd
Alex Yefimov <4lex.y3@gmail.com>
2025-10-25 23:59:35 +0200

    a

--- calendly/app/page.tsx
+++ calendly/app/page.tsx
@@ -4,6 +4,18 @@ import { InlineWidget } from "react-calendly";
 
 export default function Home() {
   return (
-    <InlineWidget styles={{overflow: "hidden"}} url="https://calendly.com/raymondo-booking-page/free-demo-call" pageSettings={{ hideEventTypeDetails: true, hideLandingPageDetails: false }} />
+    <div style={{ height: '100vh', overflow: 'hidden' }}>
+      <InlineWidget 
+        styles={{ 
+          height: '100%',
+          overflow: 'hidden'
+        }} 
+        url="https://calendly.com/raymondo-booking-page/free-demo-call" 
+        pageSettings={{ 
+          hideEventTypeDetails: true, 
+          hideLandingPageDetails: false 
+        }} 
+      />
+    </div>
   );
 }
